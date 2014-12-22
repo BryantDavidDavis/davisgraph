8a993c51d47ed5570f855fcd2a087e78a0b9bc54
BryantDavidDavis <davis.bryant@gmail.com>
2014-12-21 17:07:46 -0800

    trying to add migration of users table

---
+++ app/database/seeds/UserTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+	
+class UserTableSeeder extends Seeder {
+	
+	public function run() {
+		DB::table('users')->delete();
+		
+		$user = User::create(array(
+			'username' => 'buddyboy',
+			'password' => sha1(12345),
+			'email' => 'bryant.vietnam@gamil.com'
+		));
+		
+		$faker = Faker\Factory::create();
+		
+		for($i = 0; $i < 3; $i++) {
+			$user = User::create(array(
+				'username' => $faker->userName,
+				'password' => $faker->word,
+				'email' => $faker->email
+			));
+		}
+	}
+}
