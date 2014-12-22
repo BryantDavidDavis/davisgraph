c4bd8dae5a139010e7719ad21dbcc2e558e76e8e
BryantDavidDavis <davis.bryant@gmail.com>
2014-12-21 17:42:47 -0800

    trying again

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -25,7 +25,7 @@ $app = new Illuminate\Foundation\Application;
 */
 
 $env = $app->detectEnvironment(array(
-	'production' => array('pandorea'),
+	'production' => array('96.126.103.232'),
 	'local' => array('homestead'),
 
 ));
