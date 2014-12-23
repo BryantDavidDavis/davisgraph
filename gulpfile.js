6150bbddd38dbb00ef43cf7091ce03f7ff3c3128
BryantDavidDavis <davis.bryant@gmail.com>
2014-12-23 15:24:06 -0600

    first uglification

--- gulpfile.js
+++ gulpfile.js
@@ -5,13 +5,19 @@ var notify = require('gulp-notify');
 var sass = require('gulp-ruby-sass');
 var autoprefix = require('gulp-autoprefixer');
 var coffee = require('gulp-coffee');
+var browserify = require('gulp-browserify');
+var rename = require('gulp-rename');
+var uglify = require('gulp-uglify');
 
-var sassDir = 'app/assets/sass';
-var targetCSSdir = 'public/css';
+//var sassDir = 'app/assets/sass';
+var srcCSSDir = 'app/assets/css';
+var targetCSSDir = 'public/css';
 
-var coffeeDir = 'app/assets/coffee';
+//var coffeeDir = 'app/assets/coffee';
+var srcJSDir = 'app/assets/js';
 var targetJSDir = 'public/js';
 
+/*
 
 gulp.task('css', function() {
 	return gulp.src(sassDir + '/main.sass')
@@ -19,18 +25,20 @@ gulp.task('css', function() {
 		.pipe(gulp.dest(targetCSSdir))
 		.pipe(notify('CSS compiled prefixed and compressed'));
 });
+*/
 
 
 gulp.task('js', function() {
-	return gulp.src(coffeeDir + '/**/*.coffee')
-	.pipe(coffee().on('error', gulputil.log))
+	return gulp.src(srcJSDir + '/main.js')
+	.pipe(browserify({dubug: true}))
+	.pipe(uglify())
+	.pipe(rename('/bundle.js'))
 	.pipe(gulp.dest(targetJSDir));
 });
 
-
 gulp.task('watch', function() {
-	gulp.watch(sassDir + '/**/*.sass', ['css']);
-	gulp.watch(coffeeDir + '/**/*.coffee', ['js']);
+	//gulp.watch(sassDir + '/**/*.sass', ['css']);
+	gulp.watch(srcJSDir + '/**/*.js', ['js']);
 });
 
-gulp.task('default', ['css', 'js', 'watch']);
+gulp.task('default', ['js', 'watch']);
--- public/js/bundle.js
+++ public/js/bundle.js
@@ -1,9 +1,1 @@
-(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-module.exports = function() {
-	alert('login page');
-}
-
-},{}],2:[function(require,module,exports){
-var login = require('./login')();
-
-},{"./login":1}]},{},[2]);
+!function r(n,e,t){function o(u,f){if(!e[u]){if(!n[u]){var c="function"==typeof require&&require;if(!f&&c)return c(u,!0);if(i)return i(u,!0);throw new Error("Cannot find module '"+u+"'")}var a=e[u]={exports:{}};n[u][0].call(a.exports,function(r){var e=n[u][1][r];return o(e?e:r)},a,a.exports,r,n,e,t)}return e[u].exports}for(var i="function"==typeof require&&require,u=0;u<t.length;u++)o(t[u]);return o}({1:[function(r){r("./login")()},{"./login":2}],2:[function(r,n){n.exports=function(){alert("login page")}},{}]},{},[1]);
