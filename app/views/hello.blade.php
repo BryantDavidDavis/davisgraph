c269be8ae5cea26c79a0afb161b90184d21a7d0c
BryantDavidDavis <davis.bryant@gmail.com>
2014-12-23 21:10:43 -0600

    about to add authentication

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,6 +1,14 @@
 @extends('layouts.default')
 
 @section('content')
-		<h1>And We are off</h1>
-		<a href="{{ route('sessions.create') }}">Sign In</a>
+	<div class="row">
+		<div class="small-12 small-centered medium-12 large-10 large-centered columns">
+			<h1 class='intro text-center'>And We're Off!</h1>
+		</div>
+	</div>
+	<div class="row">
+		<div class="small-12 small-centered medium-12 large-10 large-centered columns">
+			<h2 class='text-center'><a href="{{ route('sessions.create') }}">Sign In</a></h2>
+		</div>
+	</div>
 @stop
