2706fca222e0be15d06d6a40f4a6fa30bfd352a0
BryantDavidDavis <davis.bryant@gmail.com>
2015-01-01 16:10:08 -0600

    changed the hello page

--- app/views/hello.blade.php
+++ app/views/hello.blade.php
@@ -1,14 +1,35 @@
 @extends('layouts.default')
 
+@section('header')
+	{{ HTML::style('css/hello.css') }}
+@stop
+
 @section('content')
-	<div class="row">
-		<div class="small-12 small-centered medium-12 large-10 large-centered columns">
-			<h1 class='intro text-center'>And We're Off!</h1>
+	<div class="page" style="background-image:url(images/Roatan-Pirate-Map.jpg)">
+		<div class="off-canvas-wrap" data-offcanvas>
+			<div class="inner-wrap">
+				<nav class="tab-bar">
+					<section class="left-small">
+						<a class="left-off-canvas-toggle menu-icon" href="#"><span></span></a>
+					</section>
+					<section class="middle tab-bar-section">
+						<h1 class="title">Lost in Roatan</h1>
+					</section>
+				</nav>
+				<aside class="left-off-canvas-menu">
+					<ul class="off-canvas-list">
+						<li><a href="{{ route('sessions.create') }}">Login</a></li>
+						<li><a href="{{ route('users.create') }}">Signup</a></li>
+					</ul>
+				</aside>
+				<section class="main-section">
+				</section>
+				<a class="exit-off-canvas"></a>			
+			</div>
 		</div>
 	</div>
-	<div class="row">
-		<div class="small-12 small-centered medium-12 large-10 large-centered columns">
-			<h2 class='text-center'><a href="{{ route('sessions.create') }}">Sign In</a></h2>
-		</div>
-	</div>
+@stop
+
+@section('footer')
+	{{ HTML::script('packages/vendor/foundation/js/foundation.offcanvas.js') }}
 @stop
