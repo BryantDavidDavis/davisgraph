46e8712cf6a32d347691c06780a8d81fa0524a8b
BryantDavidDavis <davis.bryant@gmail.com>
2015-01-02 12:43:12 -0800

    had to remigrate after vagrant data loss lets hope this commit doesnt break the site

--- app/views/users/index.blade.php
+++ app/views/users/index.blade.php
@@ -6,20 +6,19 @@
 
 @section('content')
 		<ul class="small-block-grid-2 small-centered columns medium-block-grid-4 large-block-grid-6">
-		@foreach($photos as $photo)
-		<li>
-			<label>{{ $photo->title }}</label>
-			<a href="{{route('photos.show', array($photo->id)) }}">
-				<img src="{{ $photo->data }}">
-			</a>
-			<a href="#" class="button tiny split">Edit<span data-dropdown="{{'drop'.$photo->id }}"></span></a><br>
-			<ul id="{{'drop'.$photo->id }}" class="f-dropdown" data-dropdown-content>
-				<li><a href="{{ route('photos.photoDestroy', array($photo->id)) }}">Delete</a></li>
-				<li><a href="#">Something Else</a></li>
-			</ul>			
+			@if (empty($photos))
+				<p>Photo Album Empty</p>
+			@else
 			
-		</li>
-		@endforeach
+				@foreach($photos as $photo)
+				<li>
+					<a class="th th-equal" href="{{route('photos.show', array($photo->id)) }}" style="background-image: url({{$photo->data}})">
+						<span>{{ $photo->title }}</span>
+					</a>			
+				</li>
+				@endforeach
+				
+			@endif
 		</ul>		
 		<a href="{{ route('photos.create') }}">Let's add some pictures</a>
 @stop
