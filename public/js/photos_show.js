73b8b235ccda6eadd9bac0ccf83a050150f641e5
BryantDavidDavis <davis.bryant@gmail.com>
2015-01-06 02:26:44 -0800

    still working on the sizing

---
+++ public/js/photos_show.js
@@ -0,0 +1,13 @@
+/*
+
+$(document).ready( function() {
+	var img = $('.photo-show-img');
+if (img.height() >= img.width()) {
+	img.css("min-height", "90%");
+	alert('we have a long load, and the height is:' + img.height());
+} else {
+	img.css("min-width", "90%");
+	alert('we have a wide load');
+}	
+});
+*/
