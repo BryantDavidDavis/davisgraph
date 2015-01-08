ce46275693084dc6b4f1e8415eb2277a31670978
BryantDavidDavis <davis.bryant@gmail.com>
2015-01-08 14:49:12 -0800

    full width divs starting to look better

--- app/views/trip.blade.php
+++ app/views/trip.blade.php
@@ -13,15 +13,23 @@
 @stop
 
 @section('content')
+		<div class="row">
+			<div class="small-12 columns trip-intro-row">
+				Once upon a time, a family went to Roatan, Honduras...
+			</div>
+		</div>
 	@foreach($trip_photos as $trip_photo)
 		<div class="row trip-photo-row">
-			<div class="small-6 columns">
+			<div class="small-7 columns text-center">
 				<a href="{{route('photos.show', array($trip_photo->id)) }}"><img photo-id="{{$trip_photo->id}}" src="{{$trip_photo->data}}"></a>
 			</div>
-			<div class="small-6 columns text-align-left">
-				<p>Here are some comments</p>
-				<p>Here are more comments</p>
-				<p>Here are even more interesting comments, they just get better all the time</p>
+			<div class="small-5 columns text-left">
+				<i class='fi-comment'>
+					<p>Bryant: Here are some comments</p>
+					<p>Suzanne: Here are more comments</p>
+					<p>Dad: Here are even more interesting comments, they just get better all the time</p>
+					<p>Mom: Here are even more interesting comments, they just get better all the time</p>
+				</i>
 			</div>
 		</div>
 
