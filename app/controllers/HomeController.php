f85db866ca78b7aa518117440b6b5b5900bd37d9
BryantDavidDavis <davis.bryant@gmail.com>
2015-01-08 13:49:45 -0800

    now we have added pictures to the front page

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -2,19 +2,6 @@
 
 class HomeController extends BaseController {
 
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
-	
 	public function __construct() {
 		View::share('site_users', User::all());
 	}
@@ -22,8 +9,10 @@ class HomeController extends BaseController {
 	public function showWelcome() {
 		$photo = new Photo();
 		$background_img = $photo->getPopularPhoto();
-	
-		return View::make('hello', array('background_img' => $background_img));
+		$trip_photos = $photo->where('showme', 1)->get();
+		$trip_photos = $photo->getPhotoFileData($trip_photos);
+
+		return View::make('trip', array('background_img' => $background_img, 'trip_photos' => $trip_photos));
 	}
 
 }
--- app/views/trip.blade.php
+++ app/views/trip.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.default')
 
 @section('header')
+	{{HTML::style('css/home_trip.css')}}
 @stop
 
 @section('menu-options')
@@ -8,11 +9,18 @@
 		<li><label>manage photos</label></li>
 		<li><a href="{{ route('photos.create')}}">Add A Photo</a></li>
 		<li><a id="photos-delete" href="{{route('users.index')}}">Delete Photos</a></li>
-
 	@endif
 @stop
 
 @section('content')
+	@foreach($trip_photos as $trip_photo)
+		<div class="row trip-photo-row">
+			<div class="small-12 columns">
+				<a href="{{route('photos.show', array($trip_photo->id)) }}"><img photo-id="{{$trip_photo->id}}" src="{{$trip_photo->data}}"></a>
+			</div>
+		</div>
+
+	@endforeach
 @stop
 
 @section('footer')
