3bc30dc4f670698ab4d095dae9de04902e8e1b92
BryantDavidDavis <davis.bryant@gmail.com>
2015-01-07 18:05:20 -0800

    addd the migration of showme

---
+++ app/database/migrations/2015_01_07_221719_add_showme_to_photos.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddShowmeToPhotos extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('photos', function(Blueprint $table)
+		{
+			$table->boolean('showme');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('photos', function(Blueprint $table)
+		{
+			$table->dropColumn('showme');
+		});
+	}
+
+}
